bb785a06d9b00b92a355c50a2809f859b59cf6d9
huiji072 <fmti0720@gmail.com>
2022-04-13 20:49:15 +0900

    UDP packet add

--- UDP/ClientUDP.java
+++ UDP/ClientUDP.java
@@ -1,4 +1,4 @@
-package test;
+package UDP;
 
 import java.awt.*;
 import java.awt.event.*;
--- UDP/QuizeClient.java
+++ UDP/QuizeClient.java
@@ -1,4 +1,4 @@
-package test;
+package UDP;
 
 import java.io.BufferedReader;
 import java.io.IOException;
