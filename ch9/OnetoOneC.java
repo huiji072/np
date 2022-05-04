64610a9b8819f17f308e64b57d14b4a6cd283f0d
huiji072 <76933597+huiji072@users.noreply.github.com>
2022-05-04 16:30:16 +0900

    Update OnetoOneC.java

--- ch9/OnetoOneC.java
+++ ch9/OnetoOneC.java
@@ -1,123 +1,144 @@
-//package ch9;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+import java.net.*;
+
+//if client ==null idsplay.append("")
+
+public class OneToOneC extends Frame implements ActionListener{
+	
+	TextArea display;
+	TextField text;
+	Label lword;
+	BufferedWriter output;
+	BufferedReader input;
+	Socket client;
+	String clientdata = "";
+	Button reconn;
+	
+	public OneToOneC() {
+		super("í´ë¼ì´ì–¸íŠ¸");
+		display = new TextArea("", 0, 0, TextArea.SCROLLBARS_VERTICAL_ONLY);
+		display.setEditable(false);
+		add(display, BorderLayout.CENTER);
+		
+		Panel pword = new Panel(new BorderLayout());
+		lword = new Label("ëŒ€í™”ë§");
+		text = new TextField(30);
+		reconn = new Button("ì¬ì ‘ì†");
+		text.addActionListener(this);
+		reconn.addActionListener(this);
+		pword.add(lword, BorderLayout.WEST);
+		pword.add(text, BorderLayout.CENTER);
+		pword.add(reconn, BorderLayout.EAST);
+		add(pword, BorderLayout.SOUTH);
+		
+		addWindowListener(new WinListener());
+		setSize(300, 200);
+		setVisible(true);
+		
+	}
+	
+	public void runClient() {	
+		try {
+			client = new Socket(InetAddress.getLocalHost(), 5000);
+			input = new BufferedReader(new InputStreamReader(client.getInputStream()));
+			output = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
+				
+
+					while(true) {
+						String serverdata = input.readLine();
+						if(serverdata == null) {
+							display.append("ì„œë²„ì™€ì˜ ì ‘ì†ì´ ì¤‘ë‹¨");
+							break;
+						}else {
+							display.append("\n ì„œë²„ë©”ì‹œì§€ "+serverdata);
+						}
+				}
+					client.close();
+					client = null;
+
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void actionPerformed(ActionEvent ae) {
+		//í´ë¼ì´ì–¸íŠ¸ ê°’ ì¶œë ¥, ì„œë²„ì— ë³´ë‚´ê¸°
+		
+		if(ae.getSource() == reconn && client != null) {
+			return;
+		}
+		if(ae.getSource()==reconn && client == null ) {
+			ClientThread reconn = new ClientThread();
+			reconn.start();
+			return;
+		}
+		if(client == null) {
+			display.append("ì—°ê²°ëœ ì„œë²„ê°€ ì—†ìŠµë‹ˆë‹¤.");
+			display.setText("");
+			return;
+		}
+		
+		clientdata = text.getText();
+		try {
+			display.append("\ní´ë¼ì´ì–¸íŠ¸ : " + clientdata);
+			output.write(clientdata + "\r\n");
+			output.flush();
+			text.setText("");
+			
+			if(clientdata.equals("quit")) {
+				client.close();
+			}
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+		
+//		if(ae.getSource() == reconn) {
+//			if(client.isClosed()) {
+//				try {
+//					client = new Socket(InetAddress.getLocalHost(), 5000);
+//					display.setText("restart"); //test
+//					ClientThread CThread = null;
+//					CThread = new ClientThread(client, display);
+//					CThread.start();
 //
-//import java.awt.*;
-//import java.awt.event.*;
-//import java.io.*;
-//import java.net.*;
+//				}catch(IOException e) {}
+//			}else {
+//				try {
+//					client.close();
+//					display.setText("restart2"); //test
+//					this.runClient(); //ì¬ì ‘ì†
+//				}catch(IOException e) {}
 //
-//public class OneToOneC extends Frame implements ActionListener{
-//	
-//	static TextArea display;
-//	static TextField text;
-//	Label lword;
-//	BufferedWriter output;
-//	BufferedReader input;
-//	Socket client;
-//	String clientdata = "";
-//	Button reconn;
-//	
-//	public OneToOneC() {
-//		super("Å¬¶óÀÌ¾ğÆ®");
-//		display = new TextArea("", 0, 0, TextArea.SCROLLBARS_VERTICAL_ONLY);
-//		display.setEditable(false);
-//		add(display, BorderLayout.CENTER);
-//		
-//		Panel pword = new Panel(new BorderLayout());
-//		lword = new Label("´ëÈ­¸»");
-//		text = new TextField(30);
-//		text.addActionListener(this);
-//		reconn = new Button("Àç¿¬°á");
-//		
-//		pword.add(lword, BorderLayout.WEST);
-//		pword.add(text, BorderLayout.CENTER);
-//		pword.add(reconn, BorderLayout.EAST);
-//		add(pword, BorderLayout.SOUTH);
-//		
-//		addWindowListener(new WinListener());
-//		setSize(300, 200);
-//		setVisible(true);
-//		
-//	}
-//	
-//	public void runClient() {
-//		ClientThread clientThread = null;
-//		try {
-//			client = new Socket(InetAddress.getLocalHost(), 5000);
-//			clientThread = new ClientThread(this, client, display);
-//			clientThread.start();
-//			
-//		}catch(IOException e) {
-//			e.printStackTrace();
-//		}
-//	}
-//	
-//	public void actionPerformed(ActionEvent ae) {
-//		clientdata = text.getText();
-//		try {
-//			display.append("\nÅ¬¶óÀÌ¾ğÆ® : " + clientdata);
-//			output.write(clientdata + "\r\n");
-//			output.flush();
-//			
-//			text.setText("");
-//			if(clientdata.equals("quit")) {
-//				client.close();
 //			}
-//		}catch(IOException e) {
-//			e.printStackTrace();
+//			
 //		}
-//	}
-//
-//	public static void main(String[] args) {
-//		// TODO Auto-generated method stub
-//		OneToOneC c = new OneToOneC();
-//		c.runClient();
-//
-//	}
-//	
-//	class WinListener extends WindowAdapter
-//	{
-//		public void windowClosing(WindowEvent e) {
-//			System.exit(0);
-//		}
-//	}
-//
-//}
-//
-//class ClientThread extends Thread{
-//	BufferedWriter output;
-//	BufferedReader input;
-//	Socket client;
-//	TextArea display;
-//	
-//	public ClientThread(OneToOneC c, Socket s, TextArea ta) {
-//		client = s;
-//		display = ta;
-//		
-//		try {
-//			client = new Socket(InetAddress.getLocalHost(), 5000);
-//			input = new BufferedReader(new InputStreamReader(client.getInputStream()));
-//			output = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
-//		}catch(IOException e) {
-//			System.out.println(e);
-//		}
-//	}
-//	
-//	public void run() {
-//		try {
-//			while(true) {
-//				String serverdata = input.readLine();
-//				if(serverdata.equals("quit")) {
-//					display.append("\n¼­¹ö¿ÍÀÇ Á¢¼ÓÀÌ Áß´ÜµÇ¾ú½À´Ï´Ù.");
-//					output.flush();
-//					break;
-//				}else {
-//					display.append("\n¼­¹ö ¸Ş½ÃÁö : " + serverdata);
-//					output.flush();
-//				}
-//			}
-//			client.close();
-//		}catch(IOException e) {
-//			e.printStackTrace();
-//		}
-//	}
-//}
+
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		OneToOneC c = new OneToOneC();
+		c.runClient();
+	}
+	
+	class WinListener extends WindowAdapter{
+		public void windowClosing(WindowEvent e) {
+			try {
+				if(client!=null)
+					client.close();
+			}catch(IOException e2) {
+				System.out.println(e2);
+			}
+			System.exit(0);
+		}
+	}
+	class ClientThread extends Thread{
+		public void run() {
+			runClient();
+		}
+	}
+}
+
