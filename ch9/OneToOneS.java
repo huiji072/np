19d0b2ca2316b20174aabaf4db36ac7d4d8bdbd8
huiji072 <76933597+huiji072@users.noreply.github.com>
2022-05-04 16:30:45 +0900

    Update OneToOneS.java

--- ch9/OneToOneS.java
+++ ch9/OneToOneS.java
@@ -1,128 +1,140 @@
-//package ch9;
-//
-//import java.awt.*;
-//import java.awt.event.ActionEvent;
-//import java.awt.event.ActionListener;
-//import java.awt.event.WindowAdapter;
-//import java.awt.event.WindowEvent;
-//import java.io.*;
-//import java.net.*;
-//
-//public class OneToOneS extends Frame implements ActionListener{
-//	
-//	static TextArea display;
-//	TextField text;
-//	Label lword;
-//	Socket connection;
-//	BufferedWriter output;
-//	BufferedReader input;
-//	String clienddata="";
-//	String serverdata = "";
-//	
-//	public OneToOneS() {
-//		super("¼­¹ö");
-//		display = new TextArea("", 0, 0, TextArea.SCROLLBARS_VERTICAL_ONLY);
-//		display.setEditable(false);
-//		add(display, BorderLayout.CENTER);
-//		
-//		
-//		Panel pword = new Panel(new BorderLayout());
-//		lword = new Label("´ëÈ­¸»");
-//		text = new TextField(30);
-//		text.addActionListener(this);
-//		pword.add(lword, BorderLayout.WEST);
-//		pword.add(text, BorderLayout.EAST);
-//		add(pword, BorderLayout.SOUTH);
-//		
-//		addWindowListener(new WinListener());
-//		setSize(300, 200);
-//		setVisible(true);
-//	}
-//	
-//	public void runServer() {
-//		ServerSocket server;
-//		ServerThread serverThread = null;
-//		try {
-//			server = new ServerSocket(5000, 100);
-//			connection = server.accept();
-//			serverThread = new ServerThread(this, connection, display);
-//			serverThread.start();
-//			System.out.println(connection.getInetAddress().getLocalHost() + "¼­¹ö´Â Å¬¶óÀÌ¾ğÆ®¿Í ¿¬°áµÊ");
-//		}catch(IOException e) {
-//			e.printStackTrace();
-//		}
-//	}
-//	
-//	public void actionPerformed(ActionEvent ae) {
-//		serverdata = text.getText();
-//		try {
-//			display.append("\n¼­¹ö : " + serverdata);
-//			output.write(serverdata + "\r\n");
-//			output.flush();
-//			text.setText("");
-//			
-//			if(serverdata.equals("quit")){
-//				connection.close();	
-//			}
-//		}catch(IOException e) {
-//			e.printStackTrace();
-//		}
-//	}
-//
-//	public static void main(String[] args) {
-//		// TODO Auto-generated method stub
-//		OneToOneS s = new OneToOneS();
-//		s.runServer();
-//
-//	}
-//	
-//	class WinListener extends WindowAdapter{
-//		public void windowClosing(WindowEvent e) {
-//			System.exit(0);
-//		}
-//	}
-//
-//}
-//
-//class ServerThread extends Thread{
-//	Socket connection;
-//	BufferedWriter output;
-//	BufferedReader input;
-//	ServerSocket server;
-//	TextArea display;
-//	
-//	public ServerThread(OneToOneS s1, Socket s, TextArea ta) {
-//		connection = s;
-//		display = ta;
-//		try {
-//			InputStream is = connection.getInputStream();
-//			InputStreamReader isr = new InputStreamReader(is);
-//			input = new BufferedReader(isr);
-//			OutputStream os = connection.getOutputStream();
-//			OutputStreamWriter osw = new OutputStreamWriter(os);
-//			output = new BufferedWriter(osw);
-//		}catch(IOException e) {
-//			System.out.println(e);
-//		}
-//	}
-//	
-//	public void run() {
-//		try {
-//			while(true) {
-//				String clientdata = input.readLine();
-//				if(clientdata.equals("quit")) {
-//					display.append("\n Å¬¶óÀÌ¾ğÆ®¿ÍÀÇ Á¢¼ÓÀÌ Áß´ÜµÇ¾ú½À´Ï´Ù.");
-//					output.flush();
-//					break;
-//				}else {
-//					display.append("\nÅ¬¶óÀÌ¾ğÆ® ¸Ş½ÃÁö :  " + clientdata);
-//					output.flush();
-//				}
-//			}
-//			connection.close();
-//		}catch(IOException e) {
-//			e.printStackTrace();
-//		}
-//	}
-//
-//}
+
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import java.util.List;
+
+public class OneToOneS extends Frame implements ActionListener{
+	
+	TextArea display;
+	TextField text;
+	Label lword;
+	Socket connection;
+	BufferedWriter output;
+	BufferedReader input;
+	String clienddata="";
+	String serverdata = "";
+	ServerSocket server;
+	List <ServerThread> list;
+	
+	public OneToOneS() {
+		super("ì„œë²„");
+		display = new TextArea("", 0, 0, TextArea.SCROLLBARS_VERTICAL_ONLY);
+		display.setEditable(false);
+		add(display, BorderLayout.CENTER);
+		
+		
+		Panel pword = new Panel(new BorderLayout());
+		lword = new Label("ëŒ€í™”ë§");
+		text = new TextField(30);
+		text.addActionListener(this);
+		pword.add(lword, BorderLayout.WEST);
+		pword.add(text, BorderLayout.CENTER);
+		add(pword, BorderLayout.SOUTH);
+		
+		addWindowListener(new WinListener());
+		setSize(300, 200);
+		setVisible(true);
+	}
+	
+	public void runServer() {
+		ServerSocket server;
+		try {
+//			list = new ArrayList<ServerThread>();
+			server = new ServerSocket(5000, 100);
+			try {
+				while(true) {
+					ServerThread SThread = null;
+					connection = server.accept();//ì ‘ì†ìš”ì²­ì´ ì˜¬ ë•Œê¹Œì§€ ê¸°ë‹¤ë¦°ë‹¤.
+					SThread = new ServerThread(connection, display);
+					SThread.start();
+	
+				}
+			}catch(IOException e) {
+				server.close();
+				server=null;
+			}
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+		//ì»¤ë„¥ì…˜ ===null ì´ë©´ conection = socket ìŠ¤ë ˆë“œ ìƒì„±,ìŠ¤ë ˆë“œ ì‹œì‘
+	}
+	
+	public void actionPerformed(ActionEvent ae) {
+
+		if(connection == null) {
+			display.append("ì—°ê²°ëœ í´ë¼ì´ì–¸íŠ¸ê°€ ì—†ìŠµë‹ˆë‹¤.");
+			text.setText("");
+			return;
+		}
+		
+		serverdata = text.getText();
+		try {
+			display.append("\nì„œë²„ : " + serverdata);
+			output.write(serverdata + "\r\n");
+			output.flush();
+			text.setText("");
+			
+			if(serverdata.equals("quit")){
+				connection.close();	
+				connection=null;
+			}
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		OneToOneS s = new OneToOneS();
+		s.runServer();
+
+	}
+	
+	class WinListener extends WindowAdapter{
+		public void windowClosing(WindowEvent e) {
+			System.exit(0);
+		}
+	}
+
+}
+
+class ServerThread extends Thread{
+	BufferedWriter output;
+	BufferedReader input;
+	Socket connection;
+	OneToOneS sv;
+	TextArea display;
+	ServerThread(Socket socket, TextArea ta){
+		connection = socket;
+		display = ta;
+	}
+
+	public void run() {
+		try {
+			InputStream is = connection.getInputStream();
+			InputStreamReader isr = new InputStreamReader(is);
+			input = new BufferedReader(isr);
+			OutputStream os = connection.getOutputStream();
+			OutputStreamWriter osw = new OutputStreamWriter(os);
+			output = new BufferedWriter(osw);
+			
+			while(true) {
+				String clientdata = input.readLine();
+				if(clientdata == null) {
+					display.append("\n í´ë¼ì´ì–¸íŠ¸ì™€ì˜ ì ‘ì†ì´ ì¤‘ë‹¨ë˜ì—ˆìŠµë‹ˆë‹¤.");
+					output.flush();
+					break;
+				}else {
+					display.append("\ní´ë¼ì´ì–¸íŠ¸ ë©”ì‹œì§€ :  " + clientdata);
+				}
+			}
+			connection.close();
+		}catch(IOException e) {
+			System.out.println(e);
+		}
+
+	}
+}
